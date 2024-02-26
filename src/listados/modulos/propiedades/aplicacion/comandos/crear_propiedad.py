0b8c23d784740cab7b10136c15609b0855b5d902
Javier Lopez <graulopezjavier@gmail.com>
2024-02-26 21:10:33 +0000

    wip command

--- src/listados/modulos/propiedades/aplicacion/comandos/crear_propiedad.py
+++ src/listados/modulos/propiedades/aplicacion/comandos/crear_propiedad.py
@@ -1,9 +1,28 @@
 from dataclasses import dataclass, field
-from listados.seedwork.aplicacion.comandos import Comando
+from listados.modulos.propiedades.aplicacion.dto import PropiedadDTO
+from listados.modulos.propiedades.dominio.fabricas import FabricaPropiedad
+from listados.seedwork.aplicacion.comandos import Comando, ComandoHandler
 
 @dataclass
 class CrearPropiedad(Comando):
     fecha_creacion: str
     fecha_actualizacion: str
     id: str
-    #itinerarios: list[ItinerarioDTO]
+    nombre: str
+    
+
+class CrearPropiedadHandler(ComandoHandler):
+    fabrica_propiedades:
+    
+    def __init__(self):
+        self.fabrica_propiedades = FabricaPropiedad()
+    
+    def handle(self, comando: CrearPropiedad):
+        dto = PropiedadDTO(
+            id=comando.id,
+            nombre=comando.nombre,
+            fecha_creacion=comando.fecha_creacion,
+            fecha_actualizacion=comando.fecha_actualizacion,
+        )
+        
+        propiedad = self.fabrica_propiedades.crear_objeto(dto, self.mapeador)
