47ef5c7d2ecf82c4a87372e489855359f34b05d6
Javier López <graulopezjavier@gmail.com>
2024-03-11 17:34:54 -0500

    fix: auditorías inscribe los consumidores de cancelar contrato auditado

--- src/auditorias/modulos/verificacion/infraestructura/consumidores.py
+++ src/auditorias/modulos/verificacion/infraestructura/consumidores.py
@@ -1,5 +1,6 @@
 from auditorias.config.pulsar import Consumidor
 from auditorias.modulos.verificacion.aplicacion.comandos.auditar_contrato import ComandoAuditarContrato, ComandoAuditarContratoHandler
+from auditorias.modulos.verificacion.aplicacion.comandos.cancelar_contrato_auditado import ComandoCancelarContratoAuditado, ComandoCancelarContratoAuditadoHandler
 
 
 consumidores = [
@@ -7,5 +8,10 @@ consumidores = [
         topico=ComandoAuditarContrato().topic_name(),
         mensaje=ComandoAuditarContrato,
         handler=ComandoAuditarContratoHandler().handle,
+    ).start,
+    Consumidor(
+        topico=ComandoCancelarContratoAuditado().topic_name(),
+        mensaje=ComandoCancelarContratoAuditado,
+        handler=ComandoCancelarContratoAuditadoHandler().handle,
     ).start,
 ]
